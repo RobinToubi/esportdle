290b77935f2890d125da330414600d3a4bdebb52
Robin Gimenez <gimenezrobin@protonmail.com>
2024-02-20 01:44:42 +0100

    conf: add leaguepedia's picture link to authorized picture source

--- next.config.js
+++ next.config.js
@@ -1,15 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "static.lolesports.com",
-        port: "",
-        pathname: "/players/**.png",
-      },
-    ],
-  }
-};
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: 'static.lolesports.com',
+                port: '',
+                pathname: '/players/**.png',
+            },
+            {
+                protocol: 'https',
+                hostname: 'static.wikia.nocookie.net',
+                port: '',
+                pathname: '**.png',
+            },
+        ],
+    },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
